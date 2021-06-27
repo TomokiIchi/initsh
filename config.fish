f8c6724b5059e104cc90cb22cd1f4ff8438a294b
TomokiIchi <tomoki.ichikawa@gaiax.com>
2021-06-27 17:27:45 +0900

    added fish_theme to config file

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
 set fish_plugins theme peco
 set GHQ_SELECTOR peco
+set fish_theme agnoster
 
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
